ac701e7ac5e4e45ca50718518103dd273b48f6b7
nikol <@nkiritsa>
2025-12-29 13:08:57 +0200

    update sidebar version display to 29.12.2025

--- src/app/layout/component/app.sidebar.ts
+++ src/app/layout/component/app.sidebar.ts
@@ -8,7 +8,7 @@ import { AppMenu } from './app.menu';
     template: ` <div class="layout-sidebar flex flex-col h-full justify-between p-4">
   <app-menu></app-menu>
 
-  <span class="text-sm text-gray-500">Version 25.12.2025</span>
+  <span class="text-sm text-gray-500">Version 29.12.2025</span>
 </div>
 `
 })
