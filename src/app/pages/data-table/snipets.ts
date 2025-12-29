06757961007b8f9f73f6c07e61239d3eef074470
nikol <@nkiritsa>
2025-12-29 13:05:12 +0200

    enhance data-table functionality: add "yesterday" and "previous month" filters; enable localStorage support to persist filters and warehouses; update i18n files with new translations; introduce "apply" button for clearer user interactions; streamline filter logic and improve data restoration on reload

--- src/app/pages/data-table/snipets.ts
+++ src/app/pages/data-table/snipets.ts
@@ -1,5 +1,9 @@
 export const SNIPETS = [
     { labelKey: 'utils.today', value: 'today' },
+    { labelKey: 'utils.yesterday', value: 'yesterday' },
     { labelKey: 'utils.currentMonth', value: 'month' },
+    { labelKey: 'utils.previousMonth', value: 'prevMonth' },
     { labelKey: 'utils.currentYear', value: 'year' }
 ];
+
+export const SALES_FILTER_KEY = 'sales_filters';
